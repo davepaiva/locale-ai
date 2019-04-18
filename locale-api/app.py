2f85987d52a51c271d891394390422b7737d0308
Dave Paiva <34977572+davepaiva@users.noreply.github.com>
2019-04-18 15:20:01 +0530

    Update app.py

--- locale-api/app.py
+++ locale-api/app.py
@@ -1,12 +1,12 @@
-from chalice import Chalice
-import psycopg2
+from chalice import Chalice  #AWS library for packaging, deploying and managing severless APIs
+import psycopg2              #used to connect to a PSQL database and run queries
 from decimal import Decimal
 from datetime import datetime
 
 
-app = Chalice(app_name='locale-api')
+app = Chalice(app_name='locale-api')  # Init chalice app
 
-def create_conn():
+def create_conn():    #function to try to esatblish a connection the database, return the connection object
     conn = None
     try:
         conn = psycopg2.connect(dbname='XRides', user='davepaiva', password='davepaivapassword', host='locale-ai.cbx79mt5d5sd.ap-south-1.rds.amazonaws.com', port='5432')
@@ -16,10 +16,10 @@ def create_conn():
     return conn
 
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['POST'])  # POST method endpoint
 def index():
-    req_data = app.current_request.to_dict()
-    query_params = req_data['query_params']
+    req_data = app.current_request.to_dict()  #stores the http request data
+    query_params = req_data['query_params']    # stores all the data passed through querry parameters
 
     booking_id_data = int(query_params['booking_id'])
     user_id_data = int(query_params['user_id'])
@@ -41,16 +41,16 @@ def index():
     driver_id_data = int(query_params['driver_id'])
 
 
-    conn = create_conn()
+    conn = create_conn()  #call the unction to create a connection
     cur = conn.cursor()
-
+# insert the data to respective tables:
     cur.execute("INSERT INTO drivers ( driver_id, vehicle_model_id) VALUES (%s, %s) ;", (driver_id_data, vehicle_model_id_data, ))
     cur.execute("INSERT INTO riders (user_id) VALUES (%s) ;", (user_id_data, ))
     cur.execute("INSERT INTO rides (booking_id, package_id, travel_type_id, from_area_id, to_area_id, from_city_id, to_city_id, from_date, to_date, online_booking, booking_created, from_lat, from_long, to_lat, to_long) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ;", (booking_id_data, package_id_data, travel_type_id_data, from_area_id_data, to_area_id_data, from_city_id_data, to_city_id_data, from_date_data, to_date_data, online_booking_data, booking_created_data, from_lat_data, from_long_data, to_lat_data, to_long_data, ))
     cur.execute("INSERT INTO ride_info (user_id, driver_id, booking_id) VALUES (%s, %s, %s) ;", (user_id_data, driver_id_data, booking_id_data, ))
     conn.commit()
-    cur.close()
-    conn.close()
+    cur.close()  
+    conn.close()  #close connection
     
 
     return req_data
